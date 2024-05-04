272ada24a9d0a0a39c9b38442c39a2a2231eed8f
Somgester <dhruvratmele13@gmail.com>
2024-05-05 00:52:36 +0530

    made changes in admin folder

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,15 @@
+import logo from './logo.png';
+import add_icon from './add_icon.png';
+import order_icon from './order_image.png';
+import profile_icon from './profile_icon.png'
+import upload_area from './upload_image.jpg'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    add_icon,
+    order_icon,
+    profile_icon,
+    upload_area,
+    parcel_icon
+}
---
+++ admin/src/components/Sidebar/sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Sidebar.css'
+const sidebar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default sidebar
