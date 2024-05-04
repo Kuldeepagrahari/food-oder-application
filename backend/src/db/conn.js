992a6e275586f179a24962b286d4ed9f9ce43e67
Kuldeepagrahari <kuldeepagrahari9103@gmail.com>
2024-05-04 18:24:09 +0530

    sam

--- backend/src/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-// connecting to students-api db
-mongoose.connect("mongodb://localhost:27017/food_delivery").then(()=>{
-    console.log("connection with db  is successful..")
-})
-.catch((err)=>{
-    console.log(err)
-})
-
