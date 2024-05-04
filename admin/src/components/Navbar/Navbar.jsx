9c0c659d51adb5686f32afb9f1d76e4d41c02193
Somgester <dhruvratmele13@gmail.com>
2024-05-05 01:18:03 +0530

    made changes

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import "./Navbar.css";
+import { assets } from '../../assets/assets';
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='navbar'>
+            <img className='logo'  src={assets.logo} alt="" />
+            {/* <br/>
+            Admin - Panel */}
+            <img className='profile'src={assets.profile_icon} alt="" />
         </div>
     );
 };
--- admin/src/components/Sidebar/sidebar.jsx
+++ admin/src/components/Sidebar/sidebar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Sidebar.css'
+import { assets } from '../../assets/assets'
 const sidebar = () => {
   return (
-    <div>
-      
+    <div className="sidebar">
+        <div className="sidebar-options">
+            <div className="sidebar-option">
+                <img src={assets.add_icon} alt="" srcset="" />
+                <p>Add Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img src={assets.order_icon} alt="" srcset="" />
+                <p>List Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img src={assets.order_icon} alt="" srcset="" />
+                <p> Orders </p>
+            </div>
+        </div>
     </div>
   )
 }
